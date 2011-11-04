2cdcf2e4e5956541d95df6fd15e9f343e990ea31
Akshay Bhardwaj <akshay.bhardwaj@hotmail.com>
2011-11-04 19:55:20 +0000

    Added update Classifier and TrainClassifier files

--- src/Classifier.m
+++ src/Classifier.m
@@ -1,10 +1,20 @@
 function [class] = Classifier(input, parameters)
 %CLASSIFIER Summary of this function goes here
 %   Detailed explanation goes here
-    [K, Inputs, Outputs] = parameters;
-    DistanceMatrix = dist(Inputs);
-    [Y, Index] = sort(DistanceMatrix(:,input));
-    OutputNeighbor = Ouputs(Index(K));
+    Inputs = parameters.inputs;
+    Outputs = parameters.outputs;
+    K = parameters.K;
+    InputM = [Inputs', input']';
+    %% Normalising the input data
+    InputM = NormaliseData(InputM, 'minrange');
+%     for col = 1: 30
+%         %TempMedian = mean(inputs(:,col));
+%         %InputM(:,col) = (InputM(:,col)-min(InputM(:,col)))/(max(InputM(:,col))-min(InputM(:,col)));
+%         InputM(:,col) = (InputM(:,col)-median(InputM(:,col)))/(2*std(InputM(:,col))^2);
+%     end
+    DistanceMatrix = dist(InputM');
+    [~, Index] = sort(DistanceMatrix(:,length(DistanceMatrix)));
+    OutputNeighbor = Outputs(Index(2:K+1));
     Count = 0;
     for i = 1:length(OutputNeighbor)
         if (OutputNeighbor(i) == 1)
@@ -12,12 +22,10 @@ function [class] = Classifier(input, parameters)
         end
     end
     
-    if ((Count/K) > 1/2)
+    if (2*Count > K)
         class = 1;
     else
         class = 0;
-    end
-        
-    
+    end   
 end
 
--- src/TrainClassifier.m
+++ src/TrainClassifier.m
@@ -1,23 +1,46 @@
 function [ parameters ] = TrainClassifier( inputs, outputs )
 %TRAINCLASSIFIER Summary of this function goes here
 %   Detailed explanation goes here
-    K = 3;
+    %% Normalising the input data
+    inputs = NormaliseData(inputs, 'minrange');
+%     for col = 1: 30
+%         %%TempMedian = mean(inputs(:,col));
+%         %%inputs(:,col) = (inputs(:,col)-min(inputs(:,col)))/(max(inputs(:,col))-min(inputs(:,col)));
+%         inputs(:,col) = (inputs(:,col)-median(inputs(:,col)))/(2*std(inputs(:,col))^2);
+%     end
     MidVal = length(inputs)/2;
-    Train = inputs(1:MidVal);
-    Test = inputs(MidVal+1:length(inputs));
-    %DistanceMatrix = dist(Train);
-    Error = zeros(10);
-    for ItError = 1:10
+    Train = inputs(1:MidVal,:);
+    Error = zeros(100,1);
+    
+    DistanceMatrix = dist(Train');
+    for K = 1:100
         for ItTrain = 1:length(Train)
-            Class = Classifier(ItTrain, [K Train outputs(1:MidVal)]);
-            if (Class == outputs(ItTrain))
-                Error(ItError) = Error(ItError)-1;
+            [~, Index] = sort(DistanceMatrix(:,ItTrain));
+            OutputNeighbor = outputs(Index(2:K+1));
+            Count = 0;
+            for i = 1:length(OutputNeighbor)
+                if (OutputNeighbor(i) == 1)
+                    Count = Count + 1;
+                end
+            end
+
+            if (2*Count > K)
+                class = 1;
             else
-                Error(ItError) = Error(ItError)+1;
+                class = 0;
+            end
+            
+            if (class == outputs(ItTrain))
+                Error(K) = Error(K)-1;
+            else
+                Error(K) = Error(K)+1;
             end
         end
     end
     
-    plot(1:10, Error);
+    plot(Error,'g');
+    [~, parameters.K] = min(Error);
+    parameters.inputs = Train;
+    parameters.outputs = outputs(1:MidVal,:);
 end
 
