bc0bdb1f7c7ed02b60168f203af5c334937d6007
Akshay Bhardwaj <akshay.bhardwaj@hotmail.com>
2011-11-07 20:00:45 +0000

    Added TestRegressor.m for robotic arm problem

---
+++ src/robotarm/TestRegressor.m
@@ -0,0 +1,10 @@
+function output = TestRegressor( parameters, input )
+%TESTREGRESSOR Summary of this function goes here
+%   Function is used to Test the Regressor system that was developed using
+%   Train Regressor.
+%   output variable the has the estimated theta1 and theta2
+
+output = parameters'*input;
+
+end
+
