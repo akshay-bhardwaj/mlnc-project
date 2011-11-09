77a2297c50e4b12678cf9520478c44848317e040
Akshay Bhardwaj <akshay.bhardwaj@hotmail.com>
2011-11-09 16:40:20 +0000

    Completed Robotic arm problem using the matrix method, now moving to sequential learning :)

--- src/robotarm/TestRegressor.m
+++ src/robotarm/TestRegressor.m
@@ -4,8 +4,27 @@ function output = TestRegressor( parameters, input )
 %   Train Regressor.
 %   output variable the has the estimated theta1 and theta2
 
-inputTrans = [1 input]';
-output = parameters'*inputTrans;
+
+%   Initialising Parameters
+InputsMean = parameters.MeanInputs;
+OutputsMean = parameters.MeanOutputs;
+StdInputs = parameters.SdInputs;
+StdOutputs = parameters.SdOutputs;
+Wml = parameters.Wml; 
+
+ModifiedInput = [0 0];
+ModifiedInput(1) = (input(1) - InputsMean(1))/StdInputs(1);
+ModifiedInput(2) = (input(2) - InputsMean(2))/StdInputs(2);
+
+
+inputTrans = [1, ModifiedInput]';
+ModifiedOutput = Wml'*inputTrans;
+output = [0 0];
+output(1) = OutputsMean(1) + StdOutputs(1)*ModifiedOutput(1);
+output(2) = OutputsMean(2) + StdOutputs(2)*ModifiedOutput(2);
+
+%inputTrans = [1 input]';
+%output = parameters'*inputTrans;
 
 end
 
--- src/robotarm/TrainRegressor.m
+++ src/robotarm/TrainRegressor.m
@@ -1,5 +1,18 @@
-function Wml = TrainRegressor(inputs, outputs)
+function parameters = TrainRegressor(inputs, outputs)
 length(inputs)
+
+InputsMean = mean(inputs);
+OutputsMean = mean(outputs);
+StdInputs = std(inputs);
+StdOutputs = std(outputs);
+
+%inputs = (inputs - InputsMean)/StdInputs;
+inputs(:,1) = inputs(:,1) - InputsMean(1);
+inputs(:,2) = inputs(:,2) - InputsMean(2);
+outputs(:,1) = outputs(:,1) - OutputsMean(1);
+outputs(:,2) = outputs(:,2) - OutputsMean(2);
+
+
 phi = zeros(length(inputs),3);
 phi(:,1) = 1;
 phi(:,[2:3]) = inputs;
@@ -8,8 +21,13 @@ phi(:,[2:3]) = inputs;
 
 %Wml = (phi'*phi)\phi' * outputs; %Wml = inv(phi'*phi)*phi' * outputs;
 ModifiedPhi = phi'*phi; %Used for calcuation of Wml
-lambda = 0.2;
-Wml = (lambda*eye(length(ModifiedPhi))+ModifiedPhi)\phi' * outputs;
+lambda = 100;
+parameters.MeanInputs = InputsMean;
+parameters.MeanOutputs = OutputsMean;
+parameters.SdInputs = StdInputs;
+parameters.SdOutputs = StdOutputs;
+parameters.Wml = (lambda*eye(length(ModifiedPhi))+ModifiedPhi)\phi' * outputs;
+
 %det(phi'*phi)
 %size(Wml)
 
