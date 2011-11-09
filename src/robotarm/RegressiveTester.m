7ee93cf8b660fd3da82848cfa63bd3f13aa1c7a6
Akshay Bhardwaj <akshay.bhardwaj@hotmail.com>
2011-11-09 14:53:58 +0000

    Commit Changes in Robotarm problem

--- src/robotarm/RegressiveTester.m
+++ src/robotarm/RegressiveTester.m
@@ -1,9 +1,34 @@
+%Number of samples to be generated from GenerateData function
+NoOfElements = 50;
+
 %Generate Data for training
-[outputs, inputs] = GenerateData(100);
+[outputs, inputs] = GenerateData(NoOfElements);
 
 %Use generated data for training
 parameters = TrainRegressor(inputs, outputs);
 
 %Generate data for Testion;
-[TestOutputs, TestInputs] = GenerateData(100);
+[TestOutputs, TestInputs] = GenerateData(NoOfElements);
+
+GeneratedOutputs = zeros(NoOfElements,2);
+
+SqErrorTheta = 0;
+for i = 1:length(TestInputs)
+    ComputedOutput = TestRegressor(parameters, TestInputs(i,:));
+    GeneratedOutputs(i,:) = ComputedOutput;
+    SqErrorTheta = SqErrorTheta + (TestOutputs(i,1) - ComputedOutput(1))^2 + (TestOutputs(i,2) - ComputedOutput(2))^2;
+    
+end
+
+
+MeanSqErrorTheta = sqrt(SqErrorTheta)/NoOfElements;
+
+
+
+
+   
+    
+    
+
+
 
--- src/robotarm/TrainRegressor.m
+++ src/robotarm/TrainRegressor.m
@@ -1,14 +1,14 @@
 function Wml = TrainRegressor(inputs, outputs)
-
-phi = zeros(100,3);
+length(inputs)
+phi = zeros(length(inputs),3);
 phi(:,1) = 1;
 phi(:,[2:3]) = inputs;
 %phi = phi'; Different notation's of phi
 
 
 Wml = (phi'*phi)\phi' * outputs; %Wml = inv(phi'*phi)*phi' * outputs;
-det(phi'*phi)
-size(Wml)
+%det(phi'*phi)
+%size(Wml)
 
 
 
